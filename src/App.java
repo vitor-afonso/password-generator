44e5777620af8f538f5b0bfe082eb1cb94cdfac7
Vitor Afonso <vittokm@hotmail.com>
2023-05-31 12:14:06 +0100

    create shouldHaveSymbols prompt

--- src/App.java
+++ src/App.java
@@ -5,6 +5,15 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         System.out.println("***** Welcome to Password Generator *****");
 
+        int passwordLength = getPasswordLength(scanner);
+
+        String shouldHaveSymbols = getShouldHaveSymbols(scanner);
+
+        System.out.println("Getting out! " + shouldHaveSymbols);
+
+    }
+
+    static int getPasswordLength(Scanner scanner) throws Exception {
         int passwordLength = 0;
         while (true){
             try {
@@ -12,10 +21,11 @@ public class App {
                 passwordLength = scanner.nextInt();
         
                 if (passwordLength > 5 && passwordLength <= 20){
-                    break;
+                    return passwordLength;
                 }
-    
-                System.out.println("Invalid length. Please enter a number between 5 and 20");
+                System.out.println("-------------------------------------------------------");
+                System.out.println("Invalid length! Please enter a number between 6 and 20.");
+                System.out.println("-------------------------------------------------------");
             } catch (Exception e) {
                 System.out.println("******************************************");
                 System.out.println("Invalid character! Input must be a number.");
@@ -24,8 +34,32 @@ public class App {
                 scanner.nextLine();
             }
         }
+    }
 
-        System.out.println("Getting out!");
+    static String getShouldHaveSymbols(Scanner scanner) throws Exception {
+        
+        while (true){
+            try {
+                System.out.print("Include symbols (@ # $ %) ? (y/n): ");
+                // Clear scanner buffer
+                scanner.nextLine();
+                String shouldHaveSymbols = scanner.nextLine();
 
+                if (shouldHaveSymbols.equals("y") || shouldHaveSymbols.equals("n")){
+                    return shouldHaveSymbols;
+                }
+                
+                System.out.println("-------------------------------------------------------");
+                System.out.println("Invalid letter! Please enter 'y' or 'n'.");
+                System.out.println("-------------------------------------------------------");
+                scanner.nextLine();
+            } catch (Exception e) {
+                System.out.println("******************************************");
+                System.out.println("Invalid character! Input must be a letter.");
+                System.out.println("******************************************");
+                // Clear scanner buffer
+                scanner.nextLine();
+            }
+        }
     }
 }
